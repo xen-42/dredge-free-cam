be9059ddcec26faf8fe0ad0462e77217450cf769
Nick <nicholasconnors@hotmail.ca>
2023-04-29 23:12:34 -0400

    Implemented freecam

---
+++ DredgeFreeCam/FreeCam.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace DredgeFreeCam
+{
+	public class FreeCam : MonoBehaviour
+	{
+		public static FreeCam Instance { get; private set; }
+
+		public bool IsActive { get; private set; }
+		public Camera oldCamera;
+		public Camera camera;
+
+		public float speed = 5f;
+
+		public void Awake()
+		{
+			if (Instance != null)
+			{
+				Destroy(this);
+			}
+			else
+			{
+				Instance = this;
+				camera = gameObject.AddComponent<Camera>();
+			}
+		}
+
+		public void Update()
+		{
+			if (Input.GetKeyUp(KeyCode.Semicolon))
+			{
+				IsActive = !IsActive;
+				if (IsActive)
+				{
+					oldCamera = Camera.current;
+					oldCamera.enabled = false;
+					camera.enabled = true;
+				}
+				else
+				{
+					camera.enabled = false;
+					oldCamera.enabled = true;
+				}
+			}
+
+			if (IsActive)
+			{
+				if (Input.GetKeyUp(KeyCode.W))
+				{
+					transform.position += Vector3.forward;
+				}
+				if (Input.GetKeyUp(KeyCode.S))
+				{
+					transform.position += Vector3.back;
+				}
+				if (Input.GetKeyUp(KeyCode.A))
+				{
+					transform.position += Vector3.left;
+				}
+				if (Input.GetKeyUp(KeyCode.D))
+				{
+					transform.position += Vector3.right;
+				}
+			}
+		}
+	}
+}
--- DredgeFreeCam/Plugin.cs
+++ DredgeFreeCam/Plugin.cs
@@ -1,15 +1,29 @@
 ﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
 
 namespace DredgeFreeCam
 {
 	[BepInPlugin("com.xen-42.dredge.freecam", "Free Camera", "0.0.1")]
 	[BepInProcess("DREDGE.exe")]
+	[HarmonyPatch]
 	public class Plugin : BaseUnityPlugin
 	{
 		private void Awake()
 		{
 			// Plugin startup logic
 			Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+			Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(Player), "Start")]
+		public static void Player_Start(Player __instance)
+		{
+			var freecam = new GameObject("FreeCam");
+			freecam.AddComponent<FreeCam>();
+			freecam.transform.position = __instance.transform.position;
 		}
 	}
 }
