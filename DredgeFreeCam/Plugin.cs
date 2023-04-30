ec2d029dec740f6100d15bdee38f419d10c8585b
Nick <nicholasconnors@hotmail.ca>
2023-04-29 22:16:51 -0400

    Create basic mod

---
+++ DredgeFreeCam/Plugin.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+
+namespace DredgeFreeCam
+{
+	[BepInPlugin("com.xen-42.dredge.freecam", "Free Camera", "0.0.1")]
+	[BepInProcess("DREDGE.exe")]
+	public class Plugin : BaseUnityPlugin
+	{
+		private void Awake()
+		{
+			// Plugin startup logic
+			Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+		}
+	}
+}
